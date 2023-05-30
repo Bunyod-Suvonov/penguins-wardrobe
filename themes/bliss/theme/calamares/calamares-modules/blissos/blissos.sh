86660c00d5df3cba00b9910a7251fb227d6483a4
Piero Proietti <piero.proietti@gmail.com>
2023-05-30 11:15:46 +0200

    blissos

--- themes/bliss/theme/calamares/calamares-modules/blissos/blissos.sh
+++ themes/bliss/theme/calamares/calamares-modules/blissos/blissos.sh
@@ -1,19 +1,20 @@
-#! /bin/bash
+#!/bin/bash
 
-# check to see if /blissos/data.img exists already, if not create it
-if [ ! -f "/blissos/data.img" ]; then
+CHROOT=$(mount | grep proc | grep calamares | awk '{print $3}' | sed -e "s#/proc##g")
 
-# Ask user what size they would like their data.img to be (4G,6G,8G,10G,12G,14G,16G, or other)
-size=$(zenity --entry --title="Create Bliss OS data.img" --text="Enter the size would you like your data.img to be? (4G, 8G, 12G, 16G, or other):" --entry-text "8G")
+# check to see if $CHROOT/blissos/data.img exists already, if not create it
+if [ ! -f "$CHROOT/blissos/data.img" ]; then
+	# Ask user what size they would like their data.img to be (4G,6G,8G,10G,12G,14G,16G, or other)
+	size=$(zenity --entry --title="Create Bliss OS data.img" --text="Enter the size would you like your data.img to be? (4G, 8G, 12G, 16G, or other):" --entry-text "8G")
 
-echo "Selected: $size"
-if [ "$size" == "" ]; then
-	echo "blank value found. Exiting"
-	exit 1
-fi
+	echo "Selected: $size"
+	if [ "$size" == "" ]; then
+		echo "blank value found. Exiting"
+		exit 1
+	fi
 
-# Create the data.img using the $size specified in GB
-sudo truncate -s $size /blissos/data.img
-sudo mkfs.ext4 -F -b 4096 -L "/data" /blissos/data.img
-sudo chmod 777 /blissos/data.img
+	# Create the data.img using the $size specified in GB
+	truncate -s $size $CHROOT/blissos/data.img
+	mkfs.ext4 -F -b 4096 -L "$CHROOT/data" "$CHROOT/blissos/data.img"
+	chmod 777 "$CHROOT/blissos/data.img"
 fi
