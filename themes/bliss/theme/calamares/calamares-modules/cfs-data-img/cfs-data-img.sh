b1707521e5cebf5be00f45fb9d585c2920692f15
Piero Proietti <piero.proietti@gmail.com>
2023-06-07 18:47:21 +0200

    included jon modules

--- themes/bliss/theme/calamares/calamares-modules/cfs-data-img/cfs-data-img.sh
+++ themes/bliss/theme/calamares/calamares-modules/cfs-data-img/cfs-data-img.sh
@@ -2,13 +2,15 @@
 
 CHROOT=$(mount | grep proc | grep calamares | awk '{print $3}' | sed -e "s#/proc##g")
 
-echo "cfs-data-img"
+echo "cfs-data-image"
 
 # check to see if $CHROOT/blissos/data.img exists already, if not create it
 if [ ! -f "$CHROOT/blissos/data.img" ]; then
 	# Ask user what size they would like their data.img to be (4G,6G,8G,10G,12G,14G,16G, or other)
-	size=$(zenity --entry --title="Create Bliss OS data.img" --text="Enter the size would you like your data.img to be? (4G, 8G, 12G, 16G, or other):" --entry-text "8G")
-
+	REQUIRED_PKG="zenity"
+	PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
+	echo Checking for $REQUIRED_PKG: $PKG_OK
+	size=$(dialog --title "Create Bliss OS data.img" --inputbox "Enter the size would you like your data.img to be? (4G, 8G, 12G, 16G, or other):" 8 40)
 	echo "Selected: $size"
 	if [ "$size" == "" ]; then
 		echo "blank value found. Exiting"
