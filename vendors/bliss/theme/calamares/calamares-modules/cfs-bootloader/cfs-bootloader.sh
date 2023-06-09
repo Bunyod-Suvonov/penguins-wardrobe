e4e8e4fce34a2aa6dd6922b540d6c58edb1882d8
Piero Proietti <piero.proietti@gmail.com>
2023-06-09 10:20:29 +0200

    adjustment for arch

--- vendors/bliss/theme/calamares/calamares-modules/cfs-bootloader/cfs-bootloader.sh
+++ vendors/bliss/theme/calamares/calamares-modules/cfs-bootloader/cfs-bootloader.sh
@@ -41,7 +41,14 @@ menuentry "BlissOS PC-Mode (Intel) w/ FFMPEG" {
 }
 
 EOF
-#chroot $CHROOT update-grub
-chroot $CHROOT grub-mkconfig -o /boot/grub/grub.cfg
+
+# Arch and Debian update-grub
+if [[ -z ${CHROOT} ]];
+    echo "CHROOT a is not set"
+    grub-mkconfig -o /boot/grub/grub.cfg
+else
+    chroot $CHROOT grub-mkconfig -o /boot/grub/grub.cfg
+fi
+
 fi
 fi
