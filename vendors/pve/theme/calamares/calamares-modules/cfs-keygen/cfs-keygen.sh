8d44436fce8c52f8f6f53a4ab8b71a0959c363da
Piero Proietti <piero.proietti@gmail.com>
2023-06-29 10:29:57 -0400

    copia di link ed icona pve

--- vendors/pve/theme/calamares/calamares-modules/cfs-keygen/cfs-keygen.sh
+++ vendors/pve/theme/calamares/calamares-modules/cfs-keygen/cfs-keygen.sh
@@ -4,3 +4,9 @@ CHROOT=$(mount | grep proc | grep calamares | awk '{print $3}' | sed -e "s#/proc
 
 # recreate keys
 chroot $CHROOT ssh-keygen -A
+
+# pve-link
+cp ../../../../pve/applications/eggs-pve.desktop $CHROOT/usr/share/applications/
+
+# pve-icon
+cp ../../../../pve/artwork/eggs-pve.png $CHROOT/usr/share/applications/ $CHROOT/usr/share/icons/
