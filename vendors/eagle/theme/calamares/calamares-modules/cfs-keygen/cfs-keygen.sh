38229b6cdf0a96a0baedff27c22a0069e100c816
Piero Proietti <piero.proietti@gmail.com>
2023-06-29 11:26:51 -0400

    penguins-pve link removed

--- vendors/eagle/theme/calamares/calamares-modules/cfs-keygen/cfs-keygen.sh
+++ vendors/eagle/theme/calamares/calamares-modules/cfs-keygen/cfs-keygen.sh
@@ -4,10 +4,3 @@ CHROOT=$(mount | grep proc | grep calamares | awk '{print $3}' | sed -e "s#/proc
 
 # recreate keys
 chroot $CHROOT ssh-keygen -A
-
-# penguins-pve link
-cp /usr/lib/penguins-eggs/addons/eggs/pve/artwork/eggs-pve.png $CHROOT/usr/share/icons/
-PVE_LINK=/usr/lib/penguins-eggs/addons/eggs/pve/applications/eggs-pve.desktop
-cp $PVE_LINK $CHROOT/usr/share/applications/
-cp $PVE_LINK $CHROOT/home/*/Desktop
-
